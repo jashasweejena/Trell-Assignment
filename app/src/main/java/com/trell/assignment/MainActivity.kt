3eacb906ed18348fd84eda79ce572e5e6e8b74b0
Jashaswee Jena <j.ishujena2000@gmail.com>
2020-09-29 09:19:42 +0530

    Initial commit

---
+++ app/src/main/java/com/trell/assignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.trell.assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
