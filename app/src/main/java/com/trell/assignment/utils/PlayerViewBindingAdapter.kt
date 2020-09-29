760eec6b09be7feb0fba1482b47ee1fbb57a1c69
Jashaswee Jena <j.ishujena2000@gmail.com>
2020-09-29 14:31:40 +0530

    Add custom PlayerViewBindingAdapter

---
+++ app/src/main/java/com/trell/assignment/utils/PlayerViewBindingAdapter.kt
@@ -0,0 +1,34 @@
+package com.assignment.trell.utils
+
+import android.net.Uri
+import androidx.databinding.BindingAdapter
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+
+class PlayerViewBindingAdapter {
+
+    companion object {
+
+        @JvmStatic
+        @BindingAdapter(value = ["url", "index"], requireAll = false)
+        fun PlayerView.playVideo(url: String, index: Int) {
+            val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(context, "ua")
+            val extractorSourceFactory = DefaultExtractorsFactory()
+            val mPlayer = SimpleExoPlayer.Builder(context, DefaultRenderersFactory(context)).build()
+            this.player = mPlayer;
+            mPlayer.playWhenReady = false
+//        mPlayer!!.seekTo(currentWindow, playbackPosition)
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory, extractorSourceFactory).createMediaSource(Uri.parse(url));
+
+            mPlayer.prepare(mediaSource)
+
+
+        }
+    }
+}
