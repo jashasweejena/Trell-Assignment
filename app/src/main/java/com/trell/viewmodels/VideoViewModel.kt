30dacb871a07d2f02be66181bf2b380673376e92
Jashaswee Jena <j.ishujena2000@gmail.com>
2020-09-29 09:49:13 +0530

    Add function in ViewModel to get videos from local storage

--- app/src/main/java/com/trell/viewmodels/VideoViewModel.kt
+++ app/src/main/java/com/trell/viewmodels/VideoViewModel.kt
@@ -19,4 +19,24 @@ class VideoViewModel(application: Application) : AndroidViewModel(application) {
     }
 
 
+    private fun getAllVideoPath(context: Context): MutableLiveData<List<Video?>> {
+        val uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+        val projection = arrayOf(MediaStore.Video.VideoColumns.DATA)
+        val cursor = context.contentResolver.query(uri, projection, null, null, null)
+        val videoArrayList: ArrayList<Video?> = ArrayList<Video?>()
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                val video = Video()
+                video.location = cursor.getString(0)
+
+                videoArrayList.add(video)
+            }
+            cursor.close()
+        }
+        val data = MutableLiveData<List<Video?>>()
+        data.value = videoArrayList
+        return data
+    }
+
+
 }
